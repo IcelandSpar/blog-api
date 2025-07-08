a43cd059e4aa6ea5e2404b029c19c210615b956c
IcySpar <victorasielrodriguez@gmail.com>
2025-07-08 12:45:09 -0700

    feat: add optional bio validator when registering

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -4,7 +4,7 @@ const { createAccount } = require('../controllers/registerController');
 const { registerValidator } = require('../validators/registerValidator.js');
 const prisma = require('../db/prismaClient');
 
-registerRouter.post('/', registerValidator,createAccount);
+registerRouter.post('/', registerValidator, createAccount);
 
 registerRouter.delete('/delete/:userId', async (req, res) => {
   const deletedUser = await prisma.users.delete({
--- validators/registerValidator.js
+++ validators/registerValidator.js
@@ -7,8 +7,12 @@ const registerValidator = [
     .isLength({min: 3, max: 18}).withMessage('Username must be between 4 - 18 characters long'),
   body('password').trim()
     .notEmpty().withMessage('Password must not be empty')
-    .isLength({min: 3, max: 18})
+    .isLength({min: 3, max: 18}),
+  body('bio').trim().optional()
+  .isLength({max: 255}).withMessage('Bio must be less than 255 characters')
 ];
+
+
 
 module.exports = {
   registerValidator,
